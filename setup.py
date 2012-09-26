cf4cff20f8ee3ed0344f75d95a08827b02fa18e2
fibio <fibio.tany@gmail.com>
2012-09-26 18:03:23 +0300

    Chaged setup

--- setup.py
+++ setup.py
@@ -17,15 +17,19 @@ from setuptools import setup
 
 setup(
     name='Flask-MongoObject',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/dqminh/flask-mongoobject',
     license='MIT',
-    author='dqminh',
-    author_email='dqminh89@gmail.com',
+    author='Fibio',
+    author_email='fibio.tany@gmail.com',
     description='Access MongoDB from your Flask application',
     long_description=__doc__,
-    packages=['flaskext'],
-    namespace_packages=['flaskext'],
+    # packages=['flaskext'],
+    # namespace_packages=['flaskext'],
+    py_modules=[
+        'flask_mongoobject'
+    ],
+    # test_suite='nose.collector',
     zip_safe=False,
     platforms='any',
     install_requires=[
@@ -34,7 +38,9 @@ setup(
         'pymongo',
         'trafaret',
     ],
-    test_suite='mongoobject_test.flask_mongoobject',
+    tests_require=[
+        'nose',
+    ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
