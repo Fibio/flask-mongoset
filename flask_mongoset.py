008762390c6a8acdfa0942f5d98600aae048c37f
fibio <fibio.tany@gmail.com>
2012-10-06 00:32:29 +0300

    Rebased NamespaceInjector

--- flask_mongoset.py
+++ flask_mongoset.py
@@ -685,10 +685,11 @@ class MongoSet(object):
         """
         if not hasattr(self, "db"):
             self.db = self.connection[self.app.config['MONGODB_DATABASE']]
-            self.db.add_son_manipulator(NamespaceInjector())
             if self.app.config['MONGODB_AUTOREF']:
+                self.db.add_son_manipulator(NamespaceInjector())
                 self.db.add_son_manipulator(AutoReferenceObject(self))
             else:
+                self.db.add_son_manipulator(NamespaceInjector())
                 self.db.add_son_manipulator(SavedObject())
             if self.app.config['MONGODB_AUTOINCREMENT']:
                 self.db.add_son_manipulator(AutoincrementId())
